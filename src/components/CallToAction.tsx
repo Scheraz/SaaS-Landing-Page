68047a42fa60e7f800e77fe891c798b12537d0f0
Scheraz <sherazkhanpk.sk@gmail.com>
2025-10-17 13:23:24 +0800

    Call to action design

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,3 +1,42 @@
+import Image from 'next/image'
+import helixImage from '../assets/images/helix2.png'
+import emojiStart from '../assets/images/emojistar.png'
+
 export const CallToAction = () => {
-  return null;
-};
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24 text-center">
+      <div className="container max-w-xl relative">
+        <Image
+          src={helixImage}
+          className="absolute top-6 left-[calc(100%+36px)]"
+          alt=""
+        />
+        <Image
+          src={emojiStart}
+          className="absolute -top-[120px] right-[calc(100%+24px)]"
+          alt=""
+        />
+        <h2 className="font-bold text-5xl tracking-tighter sm:text-6xl">
+          Get instant access
+        </h2>
+        <p className="text-xl text-white/70 mt-5">
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress and motivate your efforts
+        </p>
+        <form
+          action=""
+          className="mt-10 flex flex-col gap-2.5 max-w-sm mx-auto sm:flex-row"
+        >
+          <input
+            type="email"
+            placeholder="your@email.com"
+            className="h-12 bg-white/20 rounded-lg px-5 py-3.5 font-medium placeholder:text-[#9ca3af] flex-1"
+          />
+          <button className="bg-white text-black h-12 rounded-lg px-5">
+            Get access
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
